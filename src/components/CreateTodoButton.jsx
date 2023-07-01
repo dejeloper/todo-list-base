3df0aca1d9a86747239b9d8a77966f8986f5b761
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-07-01 17:14:35 -0500

    Add CreateTodoButton Component

---
+++ src/components/CreateTodoButton.jsx
@@ -0,0 +1,9 @@
+import "./CreateTodoButton.css";
+
+export const CreateTodoButton = ({ params }) => {
+  return (
+    <div className="content-create-item">
+      <button className="btn-create-item">Crear Nueva Tarea</button>
+    </div>
+  );
+};
