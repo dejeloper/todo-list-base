0efacfdaa9ef0d4e01cfbec6412723efb36a7ac4
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-07-02 09:10:30 -0500

    Add todos service

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   CreateTodoButton,
   TodoCounter,
@@ -5,35 +6,9 @@ import {
   TodoList,
   TodoSearch,
 } from "./components";
-import "./App.css";
-import { useState } from "react";
 
-const defaultTodos = [
-  { text: "Lista 1", completed: true },
-  { text: "Lista 2", completed: true },
-  { text: "Lista 3", completed: true },
-  { text: "Lista 4", completed: false },
-  { text: "Lista 5", completed: true },
-  { text: "Lista 6", completed: false },
-  { text: "Lista 7", completed: false },
-  { text: "Lista 8", completed: false },
-  { text: "Lista 9", completed: true },
-  { text: "Lista 10", completed: false },
-  { text: "Lista 11", completed: true },
-  { text: "Lista 12", completed: false },
-  { text: "Lista 13", completed: true },
-  { text: "Lista 14", completed: false },
-  { text: "Lista 15", completed: true },
-  { text: "Lista 16", completed: false },
-  { text: "Lista 17", completed: true },
-  { text: "Lista 18", completed: false },
-  { text: "Lista 19", completed: true },
-  { text: "Lista 20", completed: false },
-  { text: "Lista 21", completed: true },
-  { text: "Lista 22", completed: false },
-  { text: "Lista 23", completed: true },
-  { text: "Lista 24", completed: true },
-];
+import { defaultTodos } from "./services/todos";
+import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState(defaultTodos);
---
+++ src/services/todos.js
@@ -0,0 +1,7 @@
+export const defaultTodos = [
+  { text: "Lista 1", completed: true },
+  { text: "Lista 2", completed: true },
+  { text: "Lista 3", completed: false },
+  { text: "Lista 4", completed: false },
+  { text: "Lista 5", completed: true },
+];
