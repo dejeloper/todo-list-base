15cff4d9abf15ebbde68e1ddd4e1ff436430fb83
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-07-03 12:55:22 -0500

    Update Complete message on TodoCounter component

--- src/components/TodoCounter.jsx
+++ src/components/TodoCounter.jsx
@@ -3,9 +3,19 @@ import "./TodoCounter.css";
 export const TodoCounter = ({ total, completed }) => {
   return (
     <div className="content-todo-counter">
-      <h2 className="title-todo-counter">
-        Has completado <span>{completed}</span> de <span>{total}</span> tareas
-      </h2>
+      {completed === total ? (
+        <>
+          <h2 className="title-todo-counter">
+            Felicitaciones!
+            <br />
+            Completaste tus {total} TODOs 🥳 🎉🎉
+          </h2>
+        </>
+      ) : (
+        <h2 className="title-todo-counter">
+          Has completado <span>{completed}</span> de <span>{total}</span> tareas
+        </h2>
+      )}
     </div>
   );
 };
